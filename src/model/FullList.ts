417c108dde0b137837a78d5346de2ac4460947bb
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-12-10 22:18:10 -0300

    Add FullList class

---
+++ src/model/FullList.ts
@@ -0,0 +1,50 @@
+import ListItem from "./ListItem";
+
+interface List {
+  list: ListItem[];
+  load(): void;
+  save(): void;
+  clearList(): void;
+  addItem(itemObj: ListItem): void;
+  removeItem(id: string): void;
+}
+
+export default class FullList implements List {
+  static instance: FullList = new FullList();
+  private constructor(private _list: ListItem[] = []) {}
+  get list(): ListItem[] {
+    return this._list;
+  }
+
+  load(): void {
+    const list: string | null = localStorage.getItem("myList");
+    if (list) {
+      const parsedList: { _id: string; _item: string; _checked: boolean }[] =
+        JSON.parse(list);
+
+      parsedList.forEach((item) => {
+        const newListItem = new ListItem(item._id, item._item, item._checked);
+        FullList.instance.addItem(newListItem);
+      });
+    }
+  }
+
+  save(): void {
+    localStorage.setItem("myList", JSON.stringify(this._list));
+  }
+
+  clearList(): void {
+    this._list = [];
+    this.save();
+  }
+
+  addItem(itemObj: ListItem): void {
+    this._list.push(itemObj);
+    this.save();
+  }
+
+  removeItem(id: string): void {
+    this._list = this._list.filter((item) => item.id !== id);
+    this.save();
+  }
+}
