2a32137e76727aa26041d613fdf8c55d209dc318
Tamara Antonella Frazzetta <tamarafrazzetta@gmail.com>
2022-12-10 22:35:36 -0300

    Add base initApp function

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
-import './input.css'
+import "./input.css";
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItem";
+import ListTemplate from "./templates/ListTemplate";
 
+const initApp = (): void => {
+  const fullList = FullList.instance;
+  const template = ListTemplate.instance;
+
+  const itemEntryForm = document.getElementById(
+    "itemEntryForm"
+  ) as HTMLFormElement;
+  itemEntryForm.addEventListener("submit", (e: SubmitEvent) => {
+    e.preventDefault();
+  });
+  const clearItems = document.getElementById("clearItems") as HTMLButtonElement;
+  clearItems.addEventListener("click", () => { });
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
